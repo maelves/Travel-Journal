ed88cec0075db5fceeefd6e5df72b5f13561ff86
maruseanicoara <ioana.maru00@gmail.com>
2021-07-15 18:04:40 +0300

    added icon and splash screen

--- app/src/main/java/com/mnicoara/traveljournal/HomeActivity.java
+++ app/src/main/java/com/mnicoara/traveljournal/HomeActivity.java
@@ -4,16 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
 
+        startActivity(new Intent(HomeActivity.this, MainActivity.class));
+        finish();
     }
 }
